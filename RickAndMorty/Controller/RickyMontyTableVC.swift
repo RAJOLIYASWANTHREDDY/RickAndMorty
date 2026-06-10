990cac2aec3cc5242d42cb5f052b511b501a2ac6
YASWANTH <74554224+RAJOLIYASWANTHREDDY@users.noreply.github.com>
2026-06-10 08:28:09 -0700

    Created the Application for the Rick and Monty characters in table view and added each character image

---
+++ RickAndMorty/Controller/RickyMontyTableVC.swift
@@ -0,0 +1,29 @@
+//
+//  RickyMontyTableVC.swift
+//  RickAndMorty
+//
+//  Created by yaswanth reddy on 5/29/26.
+//
+
+import UIKit
+
+class RickyMontyTableVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
